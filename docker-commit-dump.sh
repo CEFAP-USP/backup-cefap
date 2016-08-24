d4f19c73984fb7f09580f33ad309dc42362cba14
tmbogus <jwgaiarsa@usp.br>
2016-08-24 17:06:46 -0300

    Syntax corrections.
    Changed timestamp format and setup.
    
    Simplified rotation routines.

--- docker-commit-dump.sh
+++ docker-commit-dump.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-if [[ $UID ne 0 ]]; then
+if [[ $UID != 0 ]]; then
     echo Error: This script is meant to be run as root.
     exit 1
 fi
 
+#Get timestamp in seconds from epoch format
+DATE=`date +%s`
+
 #Commit, dump and gzip routine
 
-docker commit -m="Backing up container $CONTAINER_NAME" -a="Cron Backup Job" $CONTAINER_ID $ORGANIZATION/cron-backup-job-$IMAGE_NAME:`date -I`
-docker export $CONTAINER_ID | gzip > $BKPATH/cron-backup-job-$IMAGE_NAME-`date -I`.tar.gz
+docker commit -m="Backing up container $CONTAINER_NAME" -a="Cron Backup Job" $CONTAINER_ID $ORGANIZATION/cron-backup-job-$IMAGE_NAME:$DATE
+docker export $CONTAINER_ID | gzip > $BKPATH/cron-backup-job-$IMAGE_NAME-$DATE.tar.gz
 
 #Rotate commits and dumps routine
+#Keeps the current and the last image and dump
 
-$NUM_IMAGES=`docker images | grep $ORGANIZATION/cron-backup-job-$IMAGE_NAME  | wc -l`
-$NUM_TARBALLS=`ls $BKPATH/cron-backup-job-$IMAGE_NAME-*.tar.gz | wc -l`
-
-if [[ $NUM_IMAGES > 2 ]]
+for IMID in `docker images -q $ORGANIZATION/cron-backup-job-$IMAGE_NAME | tail -n +3`
 do
-    for IMID in (docker images -q $ORGANIZATION/cron-backup-job-$IMAGE_NAME | head -n $(($NUM_IMAGES-2)))
-    do
-	docker rmi $IMID
-    done
-fi
+    docker rmi $IMID
+done
 
-if [[ $NUM_TARBALLS > 2 ]]
+for IMTAR in `ls -t $BKPATH/cron-backup-job-$IMAGE_NAME-*.tar.gz | tail -n +3`
 do
-    for IMTAR in (ls $BKPATH/cron-backup-job-$IMAGE_NAME-*.tar.gz | head -n $(($NUM_TARBALLS-2)))
-    do
-	rm -v $IMTAR
-    done
-fi
+    rm -v $IMTAR
+done
